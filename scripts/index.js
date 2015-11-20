0ce68f8a207cc5df17503490293c38f082467420
Ma Yanlong <mayanlong7@icloud.com>
2015-11-20 13:46:33 +0800

    logic

--- scripts/index.js
+++ scripts/index.js
@@ -15,16 +15,123 @@ window.onload = function(){
   ];
   //----------------------------------------------
   var currentIndex = 0;
-  var currentMusic = database[currentIndex];
+  var playlists;
+  var currentMusic = null;
   var currentList = null;
-  audio.src = currentMusic.src;
-
   //----------------------------------------------
+  audio.oncanplay = function(){
+    audio.play();
+  };
+  audio.onplay = function(){
+    btnplay.setAttribute('class','pause_bt');
+  };
+  audio.onpause = function(){
+    btnplay.setAttribute('class','play_bt');
+  };
+  audio.onvolumechange = function(){
+    spanvolumebar.style.width = (this.volume*100).toFixed(2) + '%';
+    spanvolumeop.style.left   = (this.volume*100).toFixed(2) + '%';
+  };
+  audio.onended = function(){
+    nextsong();
+  };
+
+  //播放进度处理
+  var handleprogress  = function(){
+    var w  = this.currentTime/this.duration;
+    var progress = (w*100).toFixed(2) + '%';
+    spanprogress_op.style.left = progress;
+    spanplaybar.style.width = progress;
+  };
+  audio.onseeked = handleprogress;
+  audio.ontimeupdate = handleprogress;
+
+  //歌曲改变处理
+  var handlesongchange = function(index){
+    if(currentList){
+      currentList.setAttribute('class','li');
+    }
+    playlists[index].setAttribute('class','li play_current');
+    currentList = playlists[index];
+    currentMusic = database[index];
+    music_name.innerHTML  = currentMusic.name;
+    singer_name.innerHTML = currentMusic.artisan;
+    ptime.innerHTML       = currentMusic.duration;
+    audio.src             =  currentMusic.src;
+  };
+  // audio.onload
+
+  //列表点击
+  divsonglist.onclick  = function(e){
+    if(e.target == this) return;
+    var el = e.target.nodeName=='LI'?e.target:e.target.parentElement;
+    currentIndex = Number(el.getAttribute('index'));
+    handlesongchange(currentIndex);
+  };
+
+  //播放暂停
+  var playpause = function(){
+    var src  = audio.getAttribute('src');
+    if(!src){
+      handlesongchange(0);
+    }
+    if(audio.paused){audio.play();}else{audio.pause();}
+  };
+  btnplay.onclick = playpause;
+
+  //下一首
+  var nextsong = function(){
+    currentIndex  += 1;
+    currentIndex   = (currentIndex == database.length)?0:currentIndex;
+    handlesongchange(currentIndex);
+  };
+  nextbt.onclick  = nextsong;
+
+  //上一首
+  var prevsong = function(){
+    currentIndex  -= 1;
+    currentIndex   = (currentIndex == -1)?database.length-1:currentIndex;
+    handlesongchange(currentIndex);
+  };
+  prevbt.onclick  = prevsong;
+
+  //点击设置音量
+  var setvolume = function(e){
+    console.log(e.layerX);
+    var pc = e.layerX/this.offsetWidth;
+    audio.volume = pc;
+  };
+
+  spanvolume.onclick = setvolume;
+  spanvolumeop.onclick = function(e){e.stopPropagation();};
+  spanprogress_op.onclick = function(e){e.stopPropagation();};
+
+  spanmute.onclick = (function(){
+    var prevolume;
+    return function(){
+      if( this.className.indexOf('icon') != -1 ){
+        this.className = 'volume_mute';
+        prevolume = audio.volume; audio.volume = 0;
+      }else{
+        this.className = 'volume_icon'; audio.volume = prevolume;
+      }
+    };
+  })();
+
+  //点击设置播放时间
+  var setcurrenttime =  function(e){
+    var pc = e.layerX/this.offsetWidth;
+    audio.currentTime = audio.duration*pc;
+  };
+  spanplayer_bgbar.parentElement.onclick  = setcurrenttime;
+
+  //创建列表
   var createlist = function(){
     for(var i = 0; i<database.length; i++){
       var el = document.createElement('li');
       el.setAttribute('src',database[i].src);
       el.setAttribute('index',i);
+      el.className = 'li';
       divsonglist.firstElementChild.appendChild(el);
       el.innerHTML = '<strong class="music_name" title="'+database[i].name+'">'+database[i].name+'</strong> ' +
         ' <strong class="singer_name" title="'+database[i].artisan+'">'+database[i].artisan+'</strong>' +
@@ -38,156 +145,7 @@ window.onload = function(){
         '  <strong class="btn_del" title="从列表中删除"> <span>删除</span> </strong> ' +
         '  </div> ';
     }
+    playlists = document.getElementsByClassName('li');
   };
-
-  divsonglist.onclick  = function(e){
-    if(e.target == this) return;
-    var el = e.target.nodeName=='LI'?e.target:e.target.parentElement;
-
-    if(currentList)
-      currentList.setAttribute('class','');
-    el.setAttribute('class','play_current');
-    currentList = el;
-
-    audio.src = el.getAttribute('src');
-    currentIndex = el.getAttribute('index');
-    currentMusic = database[currentIndex];
-
-    music_name.innerHTML = currentMusic.name;
-    singer_name.innerHTML = currentMusic.artisan;
-    ptime.innerHTML = currentMusic.duration;
-
-    audio.play();
-    btnplay.setAttribute('class','pause_bt');
-  };
-
-  var playpause = function(){
-    if(audio.paused){
-      audio.play();  this.setAttribute('class','pause_bt');
-    }else{
-      audio.pause(); this.setAttribute('class','play_bt');
-    }
-  };
-
-  var nextsong = function(){
-    if(currentList)
-      currentList.setAttribute('class','');
-    var next = currentList.nextElementSibling;
-    next = next?next:currentList.parentElement.firstElementChild;
-    next.setAttribute('class','play_current');
-    currentList = next;
-
-    currentIndex ++;
-    currentIndex = (currentIndex==database.length)?0:currentIndex;
-    currentMusic = database[currentIndex];
-    music_name.innerHTML = currentMusic.name;
-    singer_name.innerHTML = currentMusic.artisan;
-    ptime.innerHTML = currentMusic.duration;
-
-    audio.src =  currentMusic.src;
-    audio.play();
-    btnplay.setAttribute('class','pause_bt');
-  };
-
-  var prevsong = function(){
-    if(currentList)
-      currentList.setAttribute('class','');
-    var prev = currentList.previousElementSibling;
-    prev = prev?prev:currentList.parentElement.lastElementChild;
-    prev.setAttribute('class','play_current');
-    currentList = prev;
-
-    currentIndex --;
-    currentIndex = (currentIndex==-1)?database.length - 1:currentIndex;
-    currentMusic = database[currentIndex];
-
-    music_name.innerHTML = currentMusic.name;
-    singer_name.innerHTML = currentMusic.artisan;
-    ptime.innerHTML = currentMusic.duration;
-
-    audio.src =  currentMusic.src;
-    audio.play();
-    btnplay.setAttribute('class','pause_bt');
-  };
-
-  var setvolume = function(e){
-    var pc = e.layerX/this.offsetWidth;
-    audio.volume = pc;
-    var w  = (pc*100).toFixed(2) + '%';
-    spanvolumebar.style.width = w;
-    spanvolumeop.style.left = w;
-  };
-
-  var setcurrenttime =  function(e){
-    var pc = e.layerX/this.offsetWidth;
-    var pc2 = (e.layerX-3)/this.offsetWidth;
-    var w  = (pc*100).toFixed(2) + '%';
-    var w2 = (pc2*100).toFixed(2) + '%';
-    spanplaybar.style.width = w;
-    spanprogress_op.style.left = w2;
-    audio.currentTime = audio.duration*pc;
-  };
-  var timeupdatehandler = function(){
-    var pc = audio.currentTime/audio.duration;
-    var w  = (pc*100).toFixed(2) + '%';
-    spanplaybar.style.width = w;
-    spanprogress_op.style.left = w;
-    if(audio.ended){
-      nextsong();
-    }
-  };
-
-  spanmute.onclick = (function(){
-    var prevstate = {};
-    return function(){
-      if( this.className.indexOf('icon') != -1 ){
-        this.className = 'volume_mute';
-        prevstate.v = audio.volume;
-        prevstate.w = spanvolumebar.style.width;
-        prevstate.l = spanvolumeop.style.left;
-        audio.volume = 0;
-        spanvolumebar.style.width = '0%';
-        spanvolumeop.style.left = '0%';
-      }else{
-        this.className = 'volume_icon';
-        audio.volume = prevstate.v;
-        spanvolumebar.style.width = prevstate.w;
-        spanvolumeop.style.left = prevstate.l;
-      }
-    };
-  })();
-
-  spansongnum1.onclick = createlist;
-  btnplay.onclick = playpause;
-  nextbt.onclick  = nextsong;
-  prevbt.onclick  = prevsong;
-  spanvolume.onclick = setvolume;
-  spanplayer_bgbar.parentElement.onclick = setcurrenttime;
-  audio.ontimeupdate = timeupdatehandler;
-
-  // spanvolume.onmousedown = function(e){
-  //   console.log('down');
-  //   e.stopPropagation();
-  //   var that = this;
-  //   document.onmousemove = function(e){
-  //     that.parentElement.className = 'volume adjust_volume';
-  //     var pc = (e.clientX - that.getBoundingClientRect().left);
-  //     pc = Math.max(0,pc);
-  //     pc = Math.min(that.offsetWidth,pc);
-  //     var w = ((pc/that.offsetWidth)*100).toFixed(2) + '%';
-  //     // console.log(w);
-  //     spanvolumebar.style.width = w;
-  //     spanvolumeop.style.left = w;
-  //   };
-  // };
-  // spanvolume.onmouseup = function(e){
-  //   console.log('up');
-  //   e.stopPropagation();
-  //   document.onmousemove  = null;
-  // };
-  // document.onmouseup = function(e){
-  //   console.log('d_up');
-  //   document.onmousemove  = null;
-  // };
-
+  createlist();
 };
