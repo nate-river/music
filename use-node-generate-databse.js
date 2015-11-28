3a4000b2840f38d9e8eb87e75c4063a0d57b7311
Ma Yanlong <l.luffy.river@gmail.com>
2015-11-28 20:09:33 +0800

    use ffprobe to get duration

--- use-node-generate-databse.js
+++ use-node-generate-databse.js
@@ -1,4 +1,54 @@
-var fs  = require('fs');
+var fs   = require('fs');
+var exec = require('child_process').exec;
+var r = [];
+var f = [];
+var walk = function(path){
+  var files  = fs.readdirSync(path);
+  for( var i = 0; i< files.length; i++ ){
+    if( files[i] !== '.DS_Store' ){
+      var newpath = path + files[i];
+      if ( fs.statSync(newpath).isFile() ){
+        r.push( newpath );
+      }else if(fs.statSync(newpath).isDirectory() ){
+        walk( newpath+'/' ) ;
+      }
+    }
+  }
+};
+walk('./database/');
+for(var i = 0; i<r.length; i++){
+  exec('ffprobe '+ r[i], (function(i){
+    return function (error, stdout, stderr) {
+      var title =  stderr.match(/title.*/g)[0].split(':')[1].trim();
+      var artist = stderr.match(/artist.*/g)[0].split(':')[1].trim();
+      var album = stderr.match(/album.*/g)[0].split(':')[1].trim();
+      var duration = stderr.match(/Duration.[^,]*,/g)[0].slice(-8).slice(0,-4);
+      f.push({name:title,src:r[i],artisan:artist,duration:duration});
+    };
+  })(i));
+}
+setTimeout(function(){
+  var js =  'var database=' + JSON.stringify(f);
+  fs.writeFile('./scripts/database.js', js, function (err) {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  });
+},3000);
+
+
+
+
+// exec('ffprobe ./database/1.MP3', function (error, stdout, stderr) {
+//   // console.log('stdout: ' + stdout);
+//   //ffprobe
+//   // console.log('stderr: ' + stderr);
+//   var title =  stderr.match(/title.*/g)[0].split(':')[1].trim();
+//   var artist = stderr.match(/artist.*/g)[0].split(':')[1].trim();
+//   var album = stderr.match(/album.*/g)[0].split(':')[1].trim();
+//   var duration = stderr.match(/Duration.[^,]*,/g)[0].slice(-8).slice(0,-4);
+//   console.log(title,artist,album,duration);
+// });
+
 // var walk = function(path){
 //   var r = {};
 //   var files  = fs.readdirSync(path);
@@ -20,23 +70,3 @@ var fs  = require('fs');
 //   }
 //   return r;
 // };
-var r = [];
-var walk = function(path){
-  var files  = fs.readdirSync(path);
-  for( var i = 0; i< files.length; i++ ){
-    if( files[i] !== '.DS_Store' ){
-      var newpath = path + files[i];
-      if ( fs.statSync(newpath).isFile() ){
-        r.push( { name:files[i],src:newpath } );
-      }else if(fs.statSync(newpath).isDirectory() ){
-        walk( newpath+'/' ) ;
-      }
-    }
-  }
-};
-walk('./database/');
-var js =  'var database=' + JSON.stringify(r);
-fs.writeFile('./scripts/database.js', js, function (err) {
-  if (err) throw err;
-  console.log('It\'s saved!');
-});
